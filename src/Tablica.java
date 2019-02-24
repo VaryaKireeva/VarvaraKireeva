3c8c4e2cbc7fb9efef7f04bc6ca54ffe3c7da0b2
out-kireeva-vi <Pi772amr>
2019-02-24 16:07:01 +0300

    Задание 5

---
+++ src/Tablica.java
@@ -0,0 +1,38 @@
+public class Tablica {
+
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 1;
+        int c = 3;
+        int d = 4;
+        int e = 5;
+        int f = 6;
+        int g = 7;
+        int h = 8;
+        int i = 9;
+        int j = 10;
+        System.out.print("2*1=");
+        System.out.println(a*b);
+        System.out.print("2*2=");
+        System.out.println(a*a);
+        System.out.print("2*3=");
+        System.out.println(a*c);
+        System.out.print("2*4=");
+        System.out.println(a*d);
+        System.out.print("2*5=");
+        System.out.println(a*e);
+        System.out.print("2*6=");
+        System.out.println(a*f);
+        System.out.print("2*7=");
+        System.out.println(a*g);
+        System.out.print("2*8=");
+        System.out.println(a*h);
+        System.out.print("2*9=");
+        System.out.println(a*i);
+        System.out.print("2*10=");
+        System.out.println(a*j);
+
+
+
+    }
+}
