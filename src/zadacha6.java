a64699210b00ed5d026f48e92cb098ed125f7663
out-kireeva-vi <Pi772amr>
2019-02-24 17:47:16 +0300

    Задание 6

---
+++ src/zadacha6.java
@@ -0,0 +1,15 @@
+public class zadacha6 {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 2;
+        int z = 3;
+        int s = ((1+2+3)/3);
+        int d = (s/2);
+        System.out.println("Среднее арифметическое = " + s);
+        System.out.println(d);
+        if(d>3){
+            System.out.println("Программа выполнена корректно");
+        }
+
+    }
+}
