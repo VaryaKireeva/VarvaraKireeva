c2a663e4df53b5d2b510d7c14ee13ccea6973f51
out-kireeva-vi <Pi772amr>
2019-02-26 22:11:20 +0300

    Задание 8

---
+++ src/Zadanie8.java
@@ -0,0 +1,12 @@
+public class Zadanie8 {
+    public static void main(String[] args) {
+        int a = 1;
+        while (a<51){
+            System.out.println(a);
+            a++;
+
+        }
+
+
+    }
+}
