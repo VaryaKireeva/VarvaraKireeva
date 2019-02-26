bf68d03abe86453b091442c5149f8e7ddf2e1d0d
out-kireeva-vi <Pi772amr>
2019-02-26 19:04:43 +0300

    Задание 5

--- src/Tablica.java
+++ src/Tablica.java
@@ -1,37 +1,21 @@
+import java.util.Scanner;
+
 public class Tablica {
 
     public static void main(String[] args) {
-        int a = 2;
-        int b = 1;
-        int c = 3;
-        int d = 4;
-        int e = 5;
-        int f = 6;
-        int g = 7;
-        int h = 8;
-        int i = 9;
-        int j = 10;
-        System.out.print("2*1=");
-        System.out.println(a*b);
-        System.out.print("2*2=");
-        System.out.println(a*a);
-        System.out.print("2*3=");
-        System.out.println(a*c);
-        System.out.print("2*4=");
-        System.out.println(a*d);
-        System.out.print("2*5=");
-        System.out.println(a*e);
-        System.out.print("2*6=");
-        System.out.println(a*f);
-        System.out.print("2*7=");
-        System.out.println(a*g);
-        System.out.print("2*8=");
-        System.out.println(a*h);
-        System.out.print("2*9=");
-        System.out.println(a*i);
-        System.out.print("2*10=");
-        System.out.println(a*j);
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число ");
+        int a = sc.nextInt();
+        System.out.println(a + "*1" + "=" + a*1);
+        System.out.println(a + "*2" + "=" + a*2);
+        System.out.println(a + "*3" + "=" + a*3);
+        System.out.println(a + "*4" + "=" + a*4);
+        System.out.println(a + "*5" + "=" + a*5);
+        System.out.println(a + "*6" + "=" + a*6);
+        System.out.println(a + "*7" + "=" + a*7);
+        System.out.println(a + "*8" + "=" + a*8);
+        System.out.println(a + "*9" + "=" + a*9);
+        System.out.println(a + "*10" + "=" + a*10);
 
 
     }
