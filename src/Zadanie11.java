e03d16ffbc37a64de0c9f4e9dd5dab3a26b01a0f
out-kireeva-vi <Pi772amr>
2019-03-02 13:06:19 +0300

    Задание 11

---
+++ src/Zadanie11.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Zadanie11 {
+    public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+        System.out.println("Введите строку ");
+        String Stroka = a.nextLine();
+        String novayastroka = new String(Stroka);
+        System.out.println(novayastroka);
+    }
+}
