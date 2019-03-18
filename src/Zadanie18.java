7dee4abc83593cd87af1bffc5d503e4f977257d9
VaryaKireeva <varyakireeva@yandex.ru>
2019-03-18 19:29:09 +0300

    Исправленное задание 18

--- src/Zadanie18.java
+++ src/Zadanie18.java
@@ -2,29 +2,33 @@ import java.io.*;
 
 public class Zadanie18 {
     public static void main(String arg[]) {
+        int a = 0;
         String s;
-        String s2;
         try (
                 BufferedReader br = new BufferedReader(new FileReader("C:\\project\\for test.txt"))) {
             while ((s = br.readLine()) != null) {
                 System.out.println(s);
+                a++;
+
             }
         } catch (FileNotFoundException ex) {
             System.out.print("Ошибка" + ex);
         } catch (IOException ex) {
             System.out.print("Ошибка" + ex);
         }
-
+        String s2;
         try (
                 BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
-                FileWriter fileWriter = new FileWriter ("C:\\project\\for test.txt")) {
-            System.out.println("Введите текст: ");
-            s2 = br2.readLine();
-            s2 = s2 + "\r\n";
-            fileWriter.write(s2);
-        }catch (IOException ex) {
-            System.out.println("Ошибка" + ex);
-        }
+                FileWriter fileWriter = new FileWriter("C:\\project\\for test.txt")) {
+            for (; a > 0; a--) {
+                System.out.println("Введите текст: ");
+                s2 = br2.readLine();
+                s2 = s2 + "\r\n";
+                fileWriter.write(s2);
+            }
+        }catch(IOException ex){
+                System.out.println("Ошибка" + ex);
+            }
 
 
     }
